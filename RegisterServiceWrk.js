bece776559b3c61479b6b20f2974d65b10397e15
RuthMoyano <77814331+RuthMoyano@users.noreply.github.com>
2021-07-21 19:38:01 -0500

    added and fixed issues to prev code

--- RegisterServiceWrk.js
+++ RegisterServiceWrk.js
@@ -36,4 +36,57 @@ const isLocalhost = Boolean(
       });
     }
   }
+  function registerValidSW(swUrl) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                
+                console.log('New content is available; please refresh.');
+              } else {
+                
+                console.log('Content is cached for offline use.');
+              }
+            }
+          };
+        };
+      })
+      .catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
+  }
   
+  function checkValidServiceWorker(swUrl) {
+    fetch(swUrl)
+      .then(response => {
+        if (
+          response.status === 404 ||
+          response.headers.get('content-type').indexOf('javascript') === -1
+        ) {
+          navigator.serviceWorker.ready.then(registration => {
+            registration.unregister().then(() => {
+              window.location.reload();
+            });
+          });
+        } else {
+          registerValidSW(swUrl);
+        }
+      })
+      .catch(() => {
+        console.log(
+          'No internet connection found. App is running in offline mode.'
+        );
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
+  }
